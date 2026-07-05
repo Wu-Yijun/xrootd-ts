96c77271931a9e0768383c5d3ba46f97d57365aa
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2026-07-05 22:16:48 +0800

    fix

--- tests/integration/prepare.ts
+++ tests/integration/prepare.ts
@@ -2,5 +2,5 @@ import { mkdir, rm, writeFile } from "node:fs/promises";
 import { EXPECTED_FILE_CONTENTS } from "./config.ts";
 
 await rm("./mock-server/data/test", { recursive: true, force: true });
-await mkdir("./mock-server/data/test");
+await mkdir("./mock-server/data/test", { recursive: true });
 await writeFile("./mock-server/data/test/testfile.txt", EXPECTED_FILE_CONTENTS);
